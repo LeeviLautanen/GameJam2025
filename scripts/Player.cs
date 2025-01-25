52566a106275aa651063990ef9270cbb7c841f26
Faara0 <toivovanhasaari@gmail.com>
2025-01-25 20:33:34 +0200

    Lisätty boostTimer ja boostCoolDown

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,22 +1,50 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
-{
+public partial class Player : CharacterBody2D {
 	public const float Speed = 300.0f;
-	public const float BurstMultiplier = 2.0f;
-
+	public const float BoostMultiplier = 3.0f;
+	bool isBoosted = false;
+	bool isOnBoostCooldown = false;
+	
 	public override void _PhysicsProcess(double delta) {
-		
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		
 		//When "Spacebar" is pressed double speed
 		float CurrentSpeed = Speed;
-		if (Input.IsActionPressed("burst")) {
-		CurrentSpeed *= BurstMultiplier;
+		if (Input.IsActionPressed("boost") && !isBoosted && !isOnBoostCooldown) {
+			Boost();
+			BoostCoolDown();
+			} 
+		//Movement
+		if (isBoosted) {
+			CurrentSpeed = Speed * BoostMultiplier;
+		} else {
+			CurrentSpeed = Speed;
 		}
-		//Making speed consistant when movin diagonally
 		Velocity = direction * CurrentSpeed;
 		MoveAndSlide();
 	}
+	
+	// When boost is pressed call BoostTimer
+	private void Boost() {
+		var BoostTimer = GetNode<Timer>("BoostTimer");
+		BoostTimer.Timeout += OnBoostTimeout;
+		isBoosted = true;
+	}
+	//BoostTimer gives Timeout and boost ends
+	private void OnBoostTimeout() {
+		isBoosted = false;
+	}
+	
+	//Sets BoostCooldown
+	private void BoostCoolDown() {
+		var CoolDown = GetNode<Timer>("BoostCoolDown");
+		CoolDown.Timeout += BoostCoolDownTimeout;
+		isOnBoostCooldown = true;
+	}
+	
+	private void BoostCoolDownTimeout() {
+		isOnBoostCooldown = false;
+	}
+	
 }
