d82b90ee82bc7d57c236606c5d249a73ff2511a2
Leevi Lautanen <leevi.lautanen02@gmail.com>
2025-01-25 19:55:02 +0200

    Player knows which way the water flows

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,15 +6,28 @@ public partial class Player : CharacterBody2D
 {
 	public const float Speed = 200.0f;
 	public const float BurstMultiplier = 2.0f;
+	private TileMapLayer tileMap;
 
-	public override void _PhysicsProcess(double delta) {
-		
+	public override void _Ready()
+	{
+		// Get the TileMap node
+		tileMap = GetNode<TileMapLayer>("/root/Map/TheJunk");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		
+
+		// Get the tile we are on
+		Vector2I tileCoordinate = tileMap.LocalToMap(GlobalPosition);
+		Vector2I atlasIndex = tileMap.GetCellAtlasCoords(tileCoordinate);
+
 		//When "Spacebar" is pressed double speed
 		float CurrentSpeed = Speed;
-		if (Input.IsActionPressed("burst")) {
-		CurrentSpeed *= BurstMultiplier;
+		if (Input.IsActionPressed("burst"))
+		{
+			CurrentSpeed *= BurstMultiplier;
 		}
 		//Making speed consistant when movin diagonally
 		Velocity = direction * CurrentSpeed;
