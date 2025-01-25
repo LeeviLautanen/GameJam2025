628424883c58276debffa00de98fb162a285a731
Aino <aino.rakkolainen@gmail.com>
2025-01-25 23:09:53 +0200

    End animation and its assets

---
+++ scripts/EndAnimation.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class EndAnimation : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	private AnimatedSprite2D animatedSprite2D;
+	public override void _Ready()
+	{
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D.Play("end_animation");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
