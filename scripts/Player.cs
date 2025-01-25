d5b1d693a6ba3766734ac8dcaf4ca46367d8aeaa
Zapihra <iida.vaaja@gmail.com>
2025-01-25 16:07:23 +0200

    corrected broken part when merged

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -20,25 +20,3 @@ public partial class Player : CharacterBody2D
 		MoveAndSlide();
 	}
 }
-using Godot;
-using System;
-
-public partial class Player : CharacterBody2D
-{
-	public const float Speed = 300.0f;
-	public const float BurstMultiplier = 2.0f;
-
-	public override void _PhysicsProcess(double delta) {
-		//Get direction
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		
-		//When "Spacebar" is pressed double speed
-		float CurrentSpeed = Speed;
-		if (Input.IsActionPressed("burst")) {
-		CurrentSpeed *= BurstMultiplier;
-		}
-		//Movemement
-		Velocity = direction * CurrentSpeed;
-		MoveAndSlide();
-	}
-}
