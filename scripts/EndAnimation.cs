a783679b0369b188f1fc6d473f8a30eeccee79f0
Zapihra <iida.vaaja@gmail.com>
2025-01-26 11:49:17 +0200

    end scene and returning to start

--- scripts/EndAnimation.cs
+++ scripts/EndAnimation.cs
@@ -8,11 +8,15 @@ public partial class EndAnimation : Node2D
 	public override void _Ready()
 	{
 		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		animatedSprite2D.Play("end_animation");
+		animatedSprite2D.Play("end_animation"); 
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+	
+	private void _on_animated_sprite_2d_animation_finished() {
+		GetTree().CallDeferred("change_scene_to_file", "res://scenes/start.tscn");
 	}
 }
