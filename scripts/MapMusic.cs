09c48c00dd113e842e61e3b4a9ec9d3f57c15938
Viiuska <87257685+Viiuska@users.noreply.github.com>
2025-01-26 14:24:08 +0200

    game music updated

--- scripts/MapMusic.cs
+++ scripts/MapMusic.cs
@@ -6,7 +6,7 @@ public partial class MapMusic : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var audioMap = GetNode<AudioStreamPlayer2D>("GameMusic");
+		var audioMap = GetNode<AudioStreamPlayer>("GameMusic");
 		audioMap.Stream = GD.Load<AudioStream>("res://audio/Sakura-Girl-Daisy-chosic.com_.mp3");
   		audioMap.Play();
 	}
