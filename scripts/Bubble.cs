314550d6a3f80cb4918d6c109186cf9ba65c65e6
Leevi Lautanen <leevi.lautanen02@gmail.com>
2025-01-26 11:19:19 +0200

    Added small air bubble scene

---
+++ scripts/Bubble.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class Bubble : Node
+{
+	private Area2D bodyArea;
+	public override void _Ready()
+	{
+		bodyArea = GetNode<Area2D>("BodyArea");
+		GD.Print(bodyArea);
+		bodyArea.BodyEntered += OnBodyEntered;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+
+	}
+
+	private void OnBodyEntered(Node body)
+	{
+		GD.Print(body.Name);
+		if (body is Player player)
+		{
+			player.AddAir(5); // Add 5 air to the player
+			QueueFree(); // Destroy the bubble
+		}
+	}
+}
