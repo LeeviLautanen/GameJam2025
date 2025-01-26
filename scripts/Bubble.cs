7a117dc1b71d8b9eff00be1020e9693abc3940bb
Viiuska <87257685+Viiuska@users.noreply.github.com>
2025-01-26 13:47:40 +0200

    Air bubbles have audio now

--- scripts/Bubble.cs
+++ scripts/Bubble.cs
@@ -8,14 +8,23 @@ public partial class Bubble : Node
 	{
 		bodyArea = GetNode<Area2D>("BodyArea");
 		bodyArea.BodyEntered += OnBodyEntered;
+		
 	}
 
 	private void OnBodyEntered(Node body)
 	{
+		var audioAirBubble = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+		audioAirBubble.Stream = GD.Load<AudioStream>("res://audio/mixkit-liquid-bubble-3000.wav");
+  		audioAirBubble.Play();
+		
 		if (body is Player player)
 		{
 			player.AddAir(5); // Add 5 air to the player
-			QueueFree(); // Destroy the bubble
+			
+			GetTree().CreateTimer(0.15).Timeout += () =>
+		{
+			QueueFree();
+		};
 		}
 	}
 }
